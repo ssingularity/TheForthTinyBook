fafb49547935f40846c442d587a7343bc0bb7925
ssingularity <1755405701@qq.com>
2019-04-08 18:41:27 +0800

    finish neo4j

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.dao.RelationUserRepository;
+import com.example.demo.domain.RelationUser;
 import com.example.demo.service.OrderService;
+import javafx.application.Application;
+import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
+import javax.websocket.OnMessage;
+
 @SpringBootApplication
 @EnableCaching
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 	@Bean
 	RmiProxyFactoryBean service() {
 		RmiProxyFactoryBean rmiProxyFactory = new RmiProxyFactoryBean();
@@ -21,5 +32,35 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.setProperty("es.set.netty.runtime.available.processors","false");
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@Bean
+	CommandLineRunner demo(RelationUserRepository relationUserRepository) {
+		return args -> {
+		    System.out.println("start neo4j");
+			relationUserRepository.deleteAll();
+			RelationUser userA = new RelationUser("User A");
+			RelationUser userB = new RelationUser("User B");
+			RelationUser userC = new RelationUser("User C");
+
+			relationUserRepository.save(userA);
+			relationUserRepository.save(userB);
+			relationUserRepository.save(userC);
+
+			userA.follow(userB);
+			userA.follow(userC);
+			userB.follow(userC);
+			userC.follow(userB);
+
+			relationUserRepository.save(userA);
+			relationUserRepository.save(userB);
+			relationUserRepository.save(userC);
+		};
 	}
 }
---
+++ src/main/java/com/example/demo/domain/RelationUser.java
@@ -0,0 +1,50 @@
+package com.example.demo.domain;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NodeEntity
+public class RelationUser {
+    @GraphId
+    Long id;
+    String name;
+
+    @Relationship(type = "关注")
+    public Set<RelationUser> followers;
+
+    public void follow(RelationUser user){
+        if (followers == null) followers = new HashSet<>();
+        followers.add(user);
+    }
+    public RelationUser(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<RelationUser> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(Set<RelationUser> followers) {
+        this.followers = followers;
+    }
+}
